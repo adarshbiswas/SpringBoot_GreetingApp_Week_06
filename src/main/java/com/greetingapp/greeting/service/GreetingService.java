71e5da399fb15f7df82ab6d76c27b142e406bff3
adarsh biswas <officiallyadarsh2003@gmail.com>
2025-02-26 19:19:47 +0530

    [Adarsh Biswas] add: find messsage by ID

---
+++ src/main/java/com/greetingapp/greeting/service/GreetingService.java
@@ -0,0 +1,46 @@
+package com.greetingapp.greeting.service;
+
+import com.greetingapp.greeting.entities.GreetingEntity;
+import com.greetingapp.greeting.repositories.GreetingRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class GreetingService {
+
+    private final GreetingRepository greetingRepository;
+
+    // Constructor-based Dependency Injection
+    public GreetingService(GreetingRepository greetingRepository) {
+        this.greetingRepository = greetingRepository;
+    }
+
+    public String getGreetingMessage() {
+        return "Hello World";
+    }
+
+    public String customGreetingMessage(String firstName, String lastName) {
+        if (firstName != null && lastName != null) {
+            return "Hello " + firstName + " " + lastName + "!!";
+        } else if (firstName != null) {
+            return "Hello " + firstName + "!!";
+        } else if (lastName != null) {
+            return "Hello " + lastName + "!!";
+        } else {
+            return "Hello World";
+        }
+    }
+
+
+    // Save Greeting Message
+    public GreetingEntity saveGreeting(String message) {
+        GreetingEntity greeting = new GreetingEntity(message);
+        return greetingRepository.save(greeting);
+    }
+
+    // Get Greeting Message by ID
+    public Optional<GreetingEntity> getGreetingById(Long id) {
+        return greetingRepository.findById(id);
+    }
+}
